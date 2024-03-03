0e90c62795152dab39ad05e30ac544e898b4d3f3
zhuyezi531 <162005071+zhuyezi531@users.noreply.github.com>
2024-03-04 07:47:51 +0800

    first_app

--- app/main.py
+++ app/main.py
@@ -5,42 +5,54 @@ app = FastAPI()
 
 @app.get("/iris")
 def get_iris():
-    from sklearn import datasets
     import matplotlib.pyplot as plt
-    # unused but required import for doing 3d projections with matplotlib < 3.2
-    import mpl_toolkits.mplot3d  # noqa: F401
+    from mpl_toolkits.mplot3d import Axes3D
+    from sklearn import datasets
     from sklearn.decomposition import PCA
 
+    # import some data to play with
     iris = datasets.load_iris()
-    
-    _, ax = plt.subplots()
-    scatter = ax.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target)
-    ax.set(xlabel=iris.feature_names[0], ylabel=iris.feature_names[1])
-    _ = ax.legend(
-        scatter.legend_elements()[0], iris.target_names, loc="lower right", title="Classes"
-    )
+    X = iris.data[:, :2]  # we only take the first two features.
+    y = iris.target
 
+    x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+    y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+
+    plt.figure(2, figsize=(8, 6))
+    plt.clf()
+
+    # Plot the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor="k")
+    plt.xlabel("Sepal length")
+    plt.ylabel("Sepal width")
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
+
+    # To getter a better understanding of interaction of the dimensions
+    # plot the first three PCA dimensions
     fig = plt.figure(1, figsize=(8, 6))
-    ax = fig.add_subplot(111, projection="3d", elev=-150, azim=110)
-
+    ax = Axes3D(fig, elev=-150, azim=110)
     X_reduced = PCA(n_components=3).fit_transform(iris.data)
     ax.scatter(
         X_reduced[:, 0],
         X_reduced[:, 1],
         X_reduced[:, 2],
-        c=iris.target,
+        c=y,
+        cmap=plt.cm.Set1,
+        edgecolor="k",
         s=40,
     )
-
-    ax.set_title("First three PCA dimensions")
-    ax.set_xlabel("1st Eigenvector")
-    ax.xaxis.set_ticklabels([])
-    ax.set_ylabel("2nd Eigenvector")
-    ax.yaxis.set_ticklabels([])
-    ax.set_zlabel("3rd Eigenvector")
-    ax.zaxis.set_ticklabels([])
+    ax.set_title("First three PCA directions")
+    ax.set_xlabel("1st eigenvector")
+    ax.w_xaxis.set_ticklabels([])
+    ax.set_ylabel("2nd eigenvector")
+    ax.w_yaxis.set_ticklabels([])
+    ax.set_zlabel("3rd eigenvector")
+    ax.w_zaxis.set_ticklabels([])
 
     fig.savefig('iris.png')
     file = open('iris.png', mode="rb")
-
     return StreamingResponse(file, media_type="image/png")
